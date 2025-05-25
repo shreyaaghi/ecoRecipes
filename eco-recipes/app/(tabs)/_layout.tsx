cc7521b28646650125df6070c1f7e98c613325cf
Shreya Aghi <shreyaaghi@gmail.com>
2025-05-25 11:20:37 -0700

    reinstalled node modules, fixed linking context error

--- eco-recipes/app/(tabs)/_layout.tsx
+++ eco-recipes/app/(tabs)/_layout.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link } from 'expo-router';
-import { Pressable } from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { Tabs } from 'expo-router';
 import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme';
-import { useClientOnlyValue } from '@/components/useClientOnlyValue';
-import TabOneScreen from '.';
-import TabTwoScreen from './two';
-import Feather from '@expo/vector-icons/Feather';
-import AntDesign from '@expo/vector-icons/AntDesign';
-import CreateRecipeScreen from './CreateRecipe';
 
-const Tabs = createBottomTabNavigator();
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
@@ -29,25 +20,17 @@ export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs.Navigator
+    <Tabs
       screenOptions={{
         tabBarStyle: {
           backgroundColor: '#4BA9FF',
           borderTopColor: "transparent",
         },
         tabBarActiveTintColor: Colors[colorScheme ?? 'dark'].tint,
-      
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
-        // headerShown: useClientOnlyValue(false, true),
         headerShown: false,
-        // tabBarStyle: {{
-        //   backgroun
-        // }}
       }}>
       <Tabs.Screen
         name="index"
-        component={TabOneScreen}
         options={{
           title: 'Home Screen',
           tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
@@ -55,7 +38,6 @@ export default function TabLayout() {
       />
       <Tabs.Screen
         name="CreateRecipe"
-        component={CreateRecipeScreen}
         options={{
           title: 'Create Recipe',
           tabBarIcon: ({ color, size }) => <AntDesign name="pluscircle" style={{ marginBottom: -3 }} color={'#4CAF50'} size={30} />
@@ -63,12 +45,11 @@ export default function TabLayout() {
       />
       <Tabs.Screen
         name="two"
-        component={TabTwoScreen}
         options={{
           title: 'Information',
           tabBarIcon: ({ color }) => <TabBarIcon name="info-circle" color={color} />,
         }}
       />
-    </Tabs.Navigator>
+    </Tabs>
   );
 }
